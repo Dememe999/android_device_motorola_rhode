1d0eba2acacda46a4a24c331c51448f620fd37f0
Dememe999 <141191831+Dememe999@users.noreply.github.com>
2023-10-28 22:23:41 -0300

    AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_rhode.mk
+    $(LOCAL_DIR)/xdroid_rhode.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_rhode-user \
-    blaze_rhode-userdebug \
-    blaze_rhode-eng
+    xdroid_rhode-user \
+    xdroid_rhode-userdebug \
+    xdroid_rhode-eng
